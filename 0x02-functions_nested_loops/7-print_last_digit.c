b45583b16dc8c48bfb7c37baade33f0dfc599d51
Sofiannedeer <ojingirihannah22@gmail.com>
2023-08-15 15:16:10 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,31 +1,21 @@
 #include "main.h"
-/**
- * jack_bauer - prints every minute of the day for Jack
- * Return: ...
- */
-void jack_bauer(void)
-{
-int g, h, i, j;
 
-for (g = 0 ; g <= 2 ; g++)
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: The number to be treated
+ * Return: Vaule of the last digit of number
+ */
+
+int print_last_digit(int n)
+
 {
-for (h = 0 ; h <= 9 ; h++)
-{
-if ((g <= 1 && h <= 9) || (g <= 2 && h <= 3))
-{
-for (i = 0 ; i <= 5 ; i++)
-{
-for (j = 0 ; j <= 9 ; j++)
-{
-_putchar(g + '0');
-_putchar(h + '0');
-_putchar(58);
-_putchar(i + '0');
-_putchar(j + '0');
-_putchar('\n');
-}
-}
-}
-}
-}
+	int last;
+
+	last = n % 10;
+	if (last < 0)
+	{
+	last = last * -1;
+	}
+	_putchar(last + '0');
+	return (last);
 }
