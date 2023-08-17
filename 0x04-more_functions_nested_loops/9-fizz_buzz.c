7e5cd5f67738c3442e0cb12a334286b434adef1d
Sofiannedeer <ojingirihannah22@gmail.com>
2023-08-17 13:36:55 -0700

    9-fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int i;
 
 for (i = 1; i <= 100; i++)
 {
-if ((i % 3 == 0) && (i % 5 == 0))
+if ((i % 3 == 0) && (i % 5) == 0)
 {
 printf("FizzBuzz ");
 }
@@ -28,7 +28,7 @@ else
 printf("%d ", i);
 }
 }
-
+printf("Buzz");
 printf("\n");
 
 return (0);
