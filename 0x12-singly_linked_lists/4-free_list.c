f8e8ab1511e6e2b036fbfb00ebb9b178a55e4cda
Sofiannedeer <ojingirihannah22@gmail.com>
2023-09-21 03:04:01 -0700

    Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees list
+ * @head: Head of linked list
+ * Return: No return.
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current_node;
+
+	while ((current_node = head) != NULL)
+	{
+		head = head->next;
+		free(current_node->str);
+		free(current_node);
+	}
+}
